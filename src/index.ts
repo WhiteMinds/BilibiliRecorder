c27e5968a002827f997bb6aa0da5d2e4ffa7b07e
WhiteMind <WhiteMind@qq.com>
2024-03-10 17:48:03 +0800

    fix: timeoutChecker did not stop correctly

--- src/index.ts
+++ src/index.ts
@@ -235,8 +235,10 @@ function createTimeoutChecker(
   stop: () => void
 } {
   let timer: NodeJS.Timeout | null = null
+  let stopped: boolean = false
 
   const update = () => {
+    if (stopped) return
     if (timer != null) clearTimeout(timer)
     timer = setTimeout(() => {
       timer = null
@@ -249,7 +251,9 @@ function createTimeoutChecker(
   return {
     update,
     stop() {
+      stopped = true
       if (timer != null) clearTimeout(timer)
+      timer = null
     },
   }
 }
